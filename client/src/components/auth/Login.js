cf9d6f58ad805a19d346a3497d968dfde3b869e7
Huong Nguyen <huongnguyen.nth1709@gmail.com>
2022-05-11 15:02:43 +0300

    create register and login component

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,10 +1,54 @@
-import React from 'react'
+import React, {Fragment, useState} from 'react'
+import { Link } from 'react-router-dom'
 
 const Login = () => {
+    const [formData, setFormData] = useState({
+        email: '',
+        password: ''
+    })
+
+    const { email, password } = formData
+
+    const onSubmit = async (e) => {
+        e.preventDefault()
+        console.log('SUCCESS')
+    }
+
   return (
-    <section className="container">
-        <div>Login</div>
-    </section>
+      <Fragment>
+        <section className="container">
+            <h1 className="large text-primary">Sign In</h1>
+            <p className="lead"><i className="fas fa-user"></i>Sign Into Your Account</p>
+            <form className="form" onSubmit={e => onSubmit(e)}>
+                <div className="form-group">
+                <input 
+                    type="email" 
+                    placeholder="Email Address" 
+                    name="email" 
+                    value={email}
+                    onChange={e => setFormData({...formData, email: e.target.value})}
+                    required
+                />
+               
+                </div>
+                <div className="form-group">
+                  <input
+                      type="password"
+                      placeholder="Password"
+                      name="password"
+                      minLength="6"
+                      value={password}
+                      onChange={e => setFormData({...formData, password: e.target.value})}
+                  />
+                </div>
+               
+                <input type="submit" className="btn btn-primary" value="Login" />
+            </form>
+            <p className="my-1">
+                Don't have an account? <Link to='/register'>Sign Up</Link>
+            </p>
+        </section>
+      </Fragment>
   )
 }
 
